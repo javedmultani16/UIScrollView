eaddf07f7bfa9d3cb4103966942d5b3694427234
Javed <javed@qwesys.com>
2018-06-25 13:01:57 +0530

    initial commit

---
+++ scrollDemo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  scrollDemo
+//
+//  Created by ProCorner on 25/06/18.
+//  Copyright © 2018 ProCorner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        scrollView.contentSize = CGSize(width: self.view.frame.size.width, height: 1900)
+
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
